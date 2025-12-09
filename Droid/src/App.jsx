8bf748a2f96af79d107707d6e3b38bf5813743f8
kuzan <0differenthang@gmail.com>
2025-12-09 22:08:46 +0700

    modularitas src/

--- Droid/src/App.jsx
+++ Droid/src/App.jsx
@@ -1,10 +1,14 @@
+import Header from './components/header/header';
+import Main from './components/main/main';
+import Section from './components/section/section';
 
 export default function App() {
 
   return (
     <>
-     
+      <Header />
+      <Main />
+      <Section />
     </>
   )
 }
-
